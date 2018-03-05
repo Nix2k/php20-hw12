71e429a9ba7e6d9f217e8eebea2a5ac5ab3789bd
Nix2k <mihail.bokov@gmail.com>
2018-03-06 00:24:28 +0300

    v0.1

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-	require_once './db-config.php';
+	function clearInput($input) {
+		return htmlspecialchars(strip_tags($input));
+	}
 
+	require_once './db-config.php';
 	
 	try {
     	$pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
@@ -9,6 +12,17 @@
 	}
 
 	$sql = "SELECT * FROM books";
+
+	if (isset($_GET['search'])) {
+		foreach ($_GET as $key => $value) {
+			$_GET[$key] = clearInput($value);
+		}
+		if (isset($_GET['name'])) {
+			$sql = $sql . 'WHERE name=' . $_GET['name'];
+		}
+	}
+
+	
 	$data = $pdo->query($sql);
 
 ?>
@@ -21,7 +35,15 @@
     <title>Книги</title>
 </head>
 <body>
+
 <h1>Перечень книг</h1>
+
+<form action="index.php" method="GET">
+	Название: <input type="text" name="name"><br>
+	Автор: <input type="text" name="author"><br>
+	ISBN: <input type="text" name="isbn"><br>
+	<input type="submit" name="search" value="Поиск"><br>
+</form>
 
 <table>
 	<tr>
@@ -33,8 +55,13 @@
 		<th>Жанр</th>
 	</tr>
 <?php
-	foreach ($data as $row) {
-		echo "<tr><td>".$row['id']."</td><td>".$row['name']."</td><td>".$row['author']."</td><td>".$row['year']."</td><td>".$row['isbn']."</td><td>".$row['genre']."</td></tr>";
+	if ($data) {
+		foreach ($data as $row) {
+			echo "<tr><td>".$row['id']."</td><td>".$row['name']."</td><td>".$row['author']."</td><td>".$row['year']."</td><td>".$row['isbn']."</td><td>".$row['genre']."</td></tr>";
+		}
+	}
+	else {
+		echo "Нет результатов";
 	}
 ?>
 </table>
